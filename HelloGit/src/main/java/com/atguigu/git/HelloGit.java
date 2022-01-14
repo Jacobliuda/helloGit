5c461dee5c0fbf954044d1a26a2df95b81acf6ba
jacob <874944197@qq.com>
2022-01-14 19:47:35 +0800

    分支dev

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -7,5 +7,14 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git again!");
+    }
+
+    public void test(){
+        System.out.println("this is a test method!");
+    }
+
+    public void testDev(){
+        System.out.println("Code from branch dev");
     }
 }
