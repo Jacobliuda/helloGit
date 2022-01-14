5bfc617e37e8cd0b8fade892773da2b54ebac5c7
jacob <874944197@qq.com>
2022-01-14 17:28:04 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Jacob
+ * @creator 2022/1/14-17:11
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
